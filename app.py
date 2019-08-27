578ad7406a2bd4639b19f5e146df2755315f88f9
An <1274246874@qq.com>
2019-08-27 15:41:16 +0800

    增加极光推送

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 import json
 import os
 import re
+import jpush as jpush
 
 app = Flask(__name__)
 
@@ -47,6 +48,76 @@ def get_version():
     return dic[version_name]
 
 
+
+
+
+@app.route('/downloadApp',methods=['GET'])
+def download_app():
+    #dirpath = os.path.join(app.root_path, 'qiji_mu')
+    dirpath = os.getcwd()+"/qiji_mu"
+    file_name = request.args.get('file_name')
+    if not os.listdir("qiji_mu"):
+        return 'file is none'
+    else:
+        return send_from_directory( dirpath,file_name, as_attachment=True)
+
+@app.route('/getAppVersion',methods=['GET'])
+def get_app_version():
+    version_name = request.args.get('app_version')
+    dic = json.loads(readAppFile())
+    return dic[version_name]
+
+@app.route('/uploadApp',methods=['POST'])
+def upload_app():
+    f = request.files['file']
+    app_version = request.form['app_version']
+    if f :
+        respone = {'code':0,'msg':'success','data':f.filename}
+        f.save('qiji_mu/'+f.filename)
+        if not os.path.exists('version/app_version.txt'):
+            version_map = {"app_version":app_version}
+        else:
+            version_map = json.loads(readAppFile())
+            if version_map :
+                version_map = dict()
+            version_map['app_version'] = app_version
+        saveFile("version/app_version.txt",json.dumps(version_map, ensure_ascii=False))
+    else:
+        respone = {'code':-1,'msg':'fail','data':None}
+    return json.dumps(respone)
+
+
+@app.route('/pushMessage',methods=['GET'])
+def jpush_push_message():
+    content = request.args.get('content')
+    # tag = request.form['tag']
+
+    if not content:
+        respone = {'msg':"推送内容不能为空"}
+
+    # app_key和master_secret
+    app_key = '55cffc05dbe10a02cdd21ef1'
+    master_secret = 'd9bd0a5f75014667acb4a311'
+
+    _jpush = jpush.JPush(app_key, master_secret)
+    push = _jpush.create_push()
+    push.audience = jpush.all_
+    # push.notification = jpush.notification(alert=content)
+    push.message = jpush.message(msg_content=content)
+    push.platform = jpush.all_
+
+    #push.message
+
+
+    try:
+        res = push.send()
+        respone = {'code': 0, 'msg': 'success'}
+    except Exception:
+        respone = {'msg':"推送内容不能为空"}
+    return json.dumps(respone)
+    #return json.dumps(content)
+
+
 def saveFile(filename,content):
     file = open(filename, "w")
     file.write(content)
@@ -58,5 +129,13 @@ def readFile():
     file.close()
     return content
 
+
+def readAppFile():
+    file = open('version/app_version.txt','r+')
+    content = file.read()
+    file.close()
+    return content
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=82,debug=True)
+    # app.run(host='0.0.0.0',port=8000,debug=True)
+    app.run(host='127.0.0.1',port=8000,debug=True)
